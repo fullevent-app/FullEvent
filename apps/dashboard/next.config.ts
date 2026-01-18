95f337a56cd0596df0e1192827a36b10e0eced6f
Madison <madison.w.kennedy@gmail.com>
2026-01-18 11:46:40 -0600

    more dashboard fixes

--- apps/dashboard/next.config.ts
+++ apps/dashboard/next.config.ts
@@ -1,7 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  transpilePackages: ['@fullevent/react'],
+  transpilePackages: ['@fullevent/react', '@stackframe/stack'],
 };
 
 export default nextConfig;
