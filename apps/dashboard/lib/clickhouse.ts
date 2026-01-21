ad3c0a5727d142fdb51c9870b8d10527681242c8
Madison <madison.w.kennedy@gmail.com>
2026-01-21 14:18:35 -0600

    removes hard coded host URL

--- apps/dashboard/lib/clickhouse.ts
+++ apps/dashboard/lib/clickhouse.ts
@@ -1,7 +1,7 @@
 import { createClient } from '@clickhouse/client'
 
 export const clickhouse = createClient({
-    url: process.env.CLICKHOUSE_HOST || 'default',
+    url: process.env.CLICKHOUSE_HOST || 'http://localhost:8123',
     username: process.env.CLICKHOUSE_USER || 'default',
     password: process.env.CLICKHOUSE_PASSWORD,
     database: process.env.CLICKHOUSE_DB || 'default',
